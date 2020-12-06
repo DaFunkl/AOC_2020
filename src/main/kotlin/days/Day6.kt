4499fe86d15d7183341ff972492aaf17739793d4
Alexey Gasevic <gasevic.alexey@gmail.com>
2020-12-06 09:50:44 +0100

    day 6

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -1,22 +1,49 @@
 package days
 
 class Day6 : Day(6) {
-    override fun partOne(): Any{
+    override fun partOne(): Any {
         var groupAnswers = HashSet<Char>()
-        var count = 0;
-        for(line in inputList){
-            if (line.isBlank() || line == inputList.last()){
-                println(groupAnswers.size)
-                count += groupAnswers.size;
-                groupAnswers = HashSet<Char>()
+        var count = 0
+        for (line in inputList) {
+            if (line.isBlank()) {
+                count += groupAnswers.size
+                groupAnswers = HashSet()
             } else {
                 line.forEach { groupAnswers.add(it) }
             }
         }
-        return count;
+        count += groupAnswers.size
+        return count
     }
 
     override fun partTwo(): Any {
-        return -1
+        var groupAnswers = ArrayList<Set<Char>>()
+        var count = 0
+        for (line in inputList) {
+            if (line.isBlank()) {
+                val adder = if (groupAnswers.size == 1) {
+                    groupAnswers[0].size
+                } else {
+                    var intersection = groupAnswers[0]
+                    groupAnswers.forEach { intersection = intersection.intersect(it) }
+                    intersection.size
+                }
+                count += adder
+                groupAnswers = ArrayList()
+            } else {
+                val hs = HashSet<Char>()
+                line.forEach { hs.add(it) }
+                groupAnswers.add(hs)
+            }
+        }
+        val adder = if (groupAnswers.size == 1) {
+            groupAnswers[0].size
+        } else {
+            var intersection = groupAnswers[0]
+            groupAnswers.forEach { intersection = intersection.intersect(it) }
+            intersection.size
+        }
+        count += adder
+        return count
     }
 }
