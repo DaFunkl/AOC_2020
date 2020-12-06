c2c59d788d04429bbe4ed9a08056d2a63808bb80
Alexey Gasevic <gasevic.alexey@gmail.com>
2020-12-06 13:15:26 +0100

    cleanup

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -1,49 +1,37 @@
 package days
 
 class Day6 : Day(6) {
-    override fun partOne(): Any {
-        var groupAnswers = HashSet<Char>()
-        var count = 0
-        for (line in inputList) {
-            if (line.isBlank()) {
-                count += groupAnswers.size
-                groupAnswers = HashSet()
-            } else {
-                line.forEach { groupAnswers.add(it) }
-            }
-        }
-        count += groupAnswers.size
-        return count
-    }
 
-    override fun partTwo(): Any {
-        var groupAnswers = ArrayList<Set<Char>>()
-        var count = 0
+    private val answers = solve()
+
+    override fun partOne(): Any = answers.first
+
+    override fun partTwo(): Any = answers.second
+
+    private fun solve(): Pair<Int, Int> {
+        var groupAnswers = Pair(HashSet<Char>(), ArrayList<Set<Char>>())
+        var count = Pair(0, 0)
         for (line in inputList) {
             if (line.isBlank()) {
-                val adder = if (groupAnswers.size == 1) {
-                    groupAnswers[0].size
-                } else {
-                    var intersection = groupAnswers[0]
-                    groupAnswers.forEach { intersection = intersection.intersect(it) }
-                    intersection.size
-                }
-                count += adder
-                groupAnswers = ArrayList()
+                count = Pair(
+                    count.first + groupAnswers.first.size,
+                    count.second + getAdder(groupAnswers.second)
+                )
+                groupAnswers = Pair(HashSet(), ArrayList())
             } else {
                 val hs = HashSet<Char>()
-                line.forEach { hs.add(it) }
-                groupAnswers.add(hs)
+                line.forEach { groupAnswers.first.add(it); hs.add(it) }
+                groupAnswers.second.add(hs)
             }
         }
-        val adder = if (groupAnswers.size == 1) {
-            groupAnswers[0].size
-        } else {
-            var intersection = groupAnswers[0]
-            groupAnswers.forEach { intersection = intersection.intersect(it) }
-            intersection.size
-        }
-        count += adder
-        return count
+        return Pair(
+            count.first + groupAnswers.first.size,
+            count.second + getAdder(groupAnswers.second)
+        )
     }
+
+    private fun getAdder(groupAnswers: List<Set<Char>>) =
+        if (groupAnswers.size == 1) groupAnswers[0].size
+        else groupAnswers.fold(groupAnswers.first()) { acc, i -> acc.intersect(i) }.size
+
 }
