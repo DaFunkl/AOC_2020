cdecfe61aa33c3d646d403fa30042296f1201137
Alexey Gasevic <alexey.gasevic@vv.de>
2020-12-15 10:57:06 +0100

    cleanup

--- src/main/kotlin/days/Day15.kt
+++ src/main/kotlin/days/Day15.kt
@@ -8,17 +8,12 @@ class Day15 : Day(15) {
 
     private fun solve(n: Int): Any {
         val num = IntArray(n + 1)
-        val visited = BooleanArray(n + 1)
         var idx = 1
-        data.forEach {
-            num[it] = idx++
-            visited[it] = true
-        }
+        data.forEach {num[it] = idx++}
         var p = data.last()
         do {
             val pp = p
-            p = if (visited[pp]) idx - 1 - num[pp] else 0
-            visited[pp] = true
+            p = if (num[pp] != 0) idx - 1 - num[pp] else 0
             num[pp] = idx - 1
         } while (idx++ < n)
         return p
