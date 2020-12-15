c32b277ce8375faedb1c29ab4b5c81335d3d3a82
Alexey Gasevic <alexey.gasevic@vv.de>
2020-12-15 08:50:07 +0100

    cleanup

--- src/main/kotlin/days/Day15.kt
+++ src/main/kotlin/days/Day15.kt
@@ -7,22 +7,17 @@ class Day15 : Day(15) {
     override fun partTwo(): Any = solve(30000000)
 
     private fun solve(n: Int): Any {
-        val map = mutableMapOf<Int, MutableList<Int>>()
+        val m = mutableMapOf<Int, MutableList<Int>>()
         var count = 1
-        data.forEach { map[it] = mutableListOf(count++) }
-        count--
-        var previous = data.last()
+        data.forEach { m[it] = mutableListOf(count++) }; count--
+        var p = data.last()
         while (count++ < n) {
-            previous = if (map[previous]!!.size == 1) 0 else {
-                val l = map[previous]!!
-                l[l.size - 1] - l[l.size - 2]
-            }
-            if (map.containsKey(previous)) {
-                map[previous]!!.add(count)
-            } else {
-                map[previous] = mutableListOf(count)
-            }
+            p = if (m[p]!!.size == 1) 0
+            else m[p]!![m[p]!!.size - 1] - m[p]!![m[p]!!.size - 2]
+
+            if (m.containsKey(p)) m[p]!!.add(count)
+            else m[p] = mutableListOf(count)
         }
-        return previous
+        return p
     }
 }
