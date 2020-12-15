0e16617b0b2848f6d5b85c75f05b664f1e95bfbe
Alexey Gasevic <alexey.gasevic@vv.de>
2020-12-15 08:46:21 +0100

    day 15

--- src/main/kotlin/days/Day15.kt
+++ src/main/kotlin/days/Day15.kt
@@ -1,12 +1,28 @@
 package days
 
 class Day15 : Day(15) {
+    private val data = inputString.split(",").map { it.toInt() }
 
-    override fun partOne(): Any {
-        return -1
-    }
+    override fun partOne(): Any = solve(2020)
+    override fun partTwo(): Any = solve(30000000)
 
-    override fun partTwo(): Any {
-        return -1
+    private fun solve(n: Int): Any {
+        val map = mutableMapOf<Int, MutableList<Int>>()
+        var count = 1
+        data.forEach { map[it] = mutableListOf(count++) }
+        count--
+        var previous = data.last()
+        while (count++ < n) {
+            previous = if (map[previous]!!.size == 1) 0 else {
+                val l = map[previous]!!
+                l[l.size - 1] - l[l.size - 2]
+            }
+            if (map.containsKey(previous)) {
+                map[previous]!!.add(count)
+            } else {
+                map[previous] = mutableListOf(count)
+            }
+        }
+        return previous
     }
 }
---
+++ src/main/kotlin/days/Day16.kt
@@ -0,0 +1,12 @@
+package days
+
+class Day16 : Day(16) {
+
+    override fun partOne(): Any {
+        return -1
+    }
+
+    override fun partTwo(): Any {
+        return -1
+    }
+}
