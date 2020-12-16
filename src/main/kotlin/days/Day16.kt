354036580317cb24fc55122fd5a649f19be3ffd3
Alexey Gasevic <gasevic.alexey@gmail.com>
2020-12-16 10:56:35 +0100

    cleanup

--- src/main/kotlin/days/Day16.kt
+++ src/main/kotlin/days/Day16.kt
@@ -2,10 +2,10 @@ package days
 
 class Day16 : Day(16) {
 
-    var rules: MutableMap<String, IntArray> = mutableMapOf()
-    var myTicket: List<Int> = listOf()
-    var nearbyTickets: MutableList<List<Int>> = mutableListOf()
-    var invalid = BooleanArray(0)
+    private var rules: MutableMap<String, IntArray> = mutableMapOf()
+    private var myTicket: List<Int> = listOf()
+    private var nearbyTickets: MutableList<List<Int>> = mutableListOf()
+    private var invalid = BooleanArray(0)
 
     override fun partOne(): Any {
         parse()
@@ -22,7 +22,7 @@ class Day16 : Day(16) {
         return count
     }
 
-    fun isNrValid(nr: Int): Boolean {
+    private fun isNrValid(nr: Int): Boolean {
         var valid = false
         rules.forEach { (_, r) ->
             if (isNrValid(nr, r)) {
@@ -32,7 +32,7 @@ class Day16 : Day(16) {
         return valid
     }
 
-    fun isNrValid(nr: Int, r: IntArray): Boolean = nr in r[0]..r[1] || nr in r[2]..r[3]
+    private fun isNrValid(nr: Int, r: IntArray): Boolean = nr in r[0]..r[1] || nr in r[2]..r[3]
 
     override fun partTwo(): Any {
         val order = mutableMapOf<Int, String>()
@@ -44,6 +44,7 @@ class Day16 : Day(16) {
             map[i] = rules.keys.toList()
         }
 
+        // remove invalid rules
         for ((idx, ticket) in nearbyTickets.withIndex()) {
             if (invalid[idx]) continue
             for ((i, nr) in ticket.withIndex()) {
@@ -51,11 +52,12 @@ class Day16 : Day(16) {
             }
         }
 
+        // filter known rules
         val removed = mutableSetOf<String>()
         while (todos.isNotEmpty()) {
             for ((k, v) in map) {
                 if (v.size == 1) {
-                    if(removed.contains(v[0])) continue
+                    if (removed.contains(v[0])) continue
                     order[k] = v[0]
                     todos.remove(v[0])
                     removed.add(v[0])
@@ -73,7 +75,7 @@ class Day16 : Day(16) {
         return count
     }
 
-    fun parse() {
+    private fun parse() {
         var state = 0
         for (it in inputList) {
             if (it.isBlank()) continue
@@ -94,7 +96,7 @@ class Day16 : Day(16) {
                 }
                 1 -> myTicket = it.split(",").map { it.toInt() }
                 2 -> nearbyTickets.add(it.split(",").map { it.toInt() })
-                else -> throw Exception("Unkown State: $state")
+                else -> throw Exception("Unknown State: $state")
             }
         }
     }
