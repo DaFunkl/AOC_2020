5b6152d6c494fd6c1ab8a5b13257a173556876a1
Alexey Gasevic <alexey.gasevic@vv.de>
2020-12-22 10:02:43 +0100

    day 22

--- src/main/kotlin/days/Day22.kt
+++ src/main/kotlin/days/Day22.kt
@@ -1,8 +1,8 @@
 package days
 
 class Day22 : Day(22) {
-    var player1: MutableList<Int> = mutableListOf()
-    var player2: MutableList<Int> = mutableListOf()
+    private var player1: MutableList<Int> = mutableListOf()
+    private var player2: MutableList<Int> = mutableListOf()
 
     override fun partOne(): Any {
         parse()
@@ -19,71 +19,55 @@ class Day22 : Day(22) {
         return winner.reversed().withIndex().sumBy { (idx, it) -> it * (idx + 1) }
     }
 
-    val stack = mutableListOf<Int>()
 
     override fun partTwo(): Any {
         parse(); return spaceCards(player1, player2)
     }
 
-    var game = 1
-    fun spaceCards(player1: MutableList<Int>, player2: MutableList<Int>): Int {
-        val p1Set = mutableSetOf<String>(player1.joinToString())
-        val p2Set = mutableSetOf<String>(player2.joinToString())
-        var round = 1
+    private var stack = mutableListOf<Int>()
+    private fun spaceCards(player1: MutableList<Int>, player2: MutableList<Int>): Int {
+        val set = mutableSetOf<String>()
+        var p1Won = false
         while (player1.isNotEmpty() && player2.isNotEmpty()) {
-            if(game == 2 && round == 6){
-                println("asd")
+            val hash = player1.joinToString(",") + ";" + player2.joinToString(",")
+            if (set.contains(hash)) {
+                p1Won = true
+                break
             }
+            set.add(hash)
 
-            println("Round ${round++}, Game $game")
-            println("p1: ${player1.joinToString()}")
-            println("p2: ${player2.joinToString()}")
-            println()
             val c1 = player1[0]; player1.removeAt(0)
             val c2 = player2[0]; player2.removeAt(0)
-            stack.add(Math.min(c1, c2)); stack.add(Math.max(c1, c2))
-            var winner = 0
+            stack.add(c2); stack.add(c1)
 
+            val winner: Int = if (c1 <= player1.size && c2 <= player2.size) {
+                val p1l = mutableListOf<Int>(); for (i in 0 until c1) p1l.add(player1[i])
+                val p2l = mutableListOf<Int>(); for (i in 0 until c2) p2l.add(player2[i])
+                spaceCards(p1l, p2l)
+            } else if (c1 > c2) 1 else 2
 
-            if (p1Set.contains(player1.joinToString()) || p2Set.contains(player2.joinToString())) winner = 1
-            else if (c1 <= player1.size && c2 <= player2.size) {
-                val p1l = mutableListOf<Int>()
-                for (i in 0 until c1) p1l.add(player1[i])
-                val p2l = mutableListOf<Int>()
-                for (i in 0 until c2) p2l.add(player2[i])
-                game++
-                winner = spaceCards(p1l, p2l)
-            } else winner = if (c1 > c2) 1 else 2
-
-            p1Set.add(player1.joinToString()); p2Set.add(player2.joinToString())
-
-            val n1 = stack[stack.size - 1]
-            val n2 = stack[stack.size - 2]
             when (winner) {
                 1 -> {
-                    player1.add(n1); player1.add(n2)
+                    player1.add(stack[stack.size - 1]); player1.add(stack[stack.size - 2])
                 }
                 2 -> {
-                    player2.add(n1); player2.add(n2)
+                    player2.add(stack[stack.size - 2]); player2.add(stack[stack.size - 1])
                 }
-                else -> {
-                    return winner
-                }
+                else -> return winner
             }
             stack.removeLast()
             stack.removeLast()
         }
-        game--
-        if (stack.isEmpty()) {
-            val winner = if (player1.isEmpty()) player2 else player1
-            return winner.reversed().withIndex().sumBy { (idx, it) -> it * (idx + 1) }
+
+        return if (p1Won || stack.isNotEmpty()) {
+            if (p1Won || player2.isEmpty()) 1 else 2
         } else {
-            if (player1.isEmpty()) return 2
-            else return 1
+            val winner = if (player1.isEmpty()) player2 else player1
+            winner.reversed().withIndex().sumBy { (idx, it) -> it * (idx + 1) }
         }
     }
 
-    fun parse() {
+    private fun parse() {
         player1 = mutableListOf()
         player2 = mutableListOf()
         var playerIdx = 1
---
+++ src/main/kotlin/days/Day23.kt
@@ -0,0 +1,13 @@
+package days
+
+class Day23 : Day(23) {
+
+    override fun partOne(): Any {
+        return -1
+    }
+
+
+    override fun partTwo(): Any {
+        return -1
+    }
+}
