be74353c8166a582c5a43e34dc9227edd20b3f9d
Alexey Gasevic <gasevic.alexey@gmail.com>
2020-12-22 08:31:28 +0100

    qs

--- src/main/kotlin/days/Day22.kt
+++ src/main/kotlin/days/Day22.kt
@@ -19,60 +19,66 @@ class Day22 : Day(22) {
         return winner.reversed().withIndex().sumBy { (idx, it) -> it * (idx + 1) }
     }
 
-    val p1Set = mutableSetOf<List<Int>>(player1)
-    val p2Set = mutableSetOf<List<Int>>(player2)
     val stack = mutableListOf<Int>()
+
     override fun partTwo(): Any {
-        parse()
-        while (player1.isNotEmpty() && player2.isNotEmpty()) {
-            val c1 = player1[0]; player1.removeAt(0)
-            val c2 = player2[0]; player2.removeAt(0)
-            stack.add(c1); stack.add(c2)
-            var winner = 0
-
-            if (p1Set.contains(player1) || p2Set.contains(player2)) winner = 1
-            else if (c1 == player1.size || c2 == player2.size) winner = -1
-            else winner = if (c1 > c2) 1 else 2
-
-            p1Set.add(player1); p2Set.add(player2)
-
-            when (winner) {
-                1 -> player1.addAll(stack.sortedDescending())
-                2 -> player2.addAll(stack.sortedDescending())
-
-            }
-
-        }
-
-        val winner = if (player1.isEmpty()) player2 else player1
-        return winner.reversed().withIndex().sumBy { (idx, it) -> it * (idx + 1) }
+        parse(); return spaceCards(player1, player2)
     }
 
-    fun recGame(player1: MutableList<Int>, player2: MutableList<Int>): Int {
+    var game = 1
+    fun spaceCards(player1: MutableList<Int>, player2: MutableList<Int>): Int {
+        val p1Set = mutableSetOf<String>(player1.joinToString())
+        val p2Set = mutableSetOf<String>(player2.joinToString())
+        var round = 1
         while (player1.isNotEmpty() && player2.isNotEmpty()) {
+            if(game == 2 && round == 6){
+                println("asd")
+            }
+
+            println("Round ${round++}, Game $game")
+            println("p1: ${player1.joinToString()}")
+            println("p2: ${player2.joinToString()}")
+            println()
             val c1 = player1[0]; player1.removeAt(0)
             val c2 = player2[0]; player2.removeAt(0)
-            stack.add(c1); stack.add(c2)
+            stack.add(Math.min(c1, c2)); stack.add(Math.max(c1, c2))
             var winner = 0
 
-            if (p1Set.contains(player1) || p2Set.contains(player2)) winner = 1
-            else if (c1 == player1.size || c2 == player2.size) {
-                winner = recGame(mutableListOf(player1), mutableListOf(player2))
+
+            if (p1Set.contains(player1.joinToString()) || p2Set.contains(player2.joinToString())) winner = 1
+            else if (c1 <= player1.size && c2 <= player2.size) {
+                val p1l = mutableListOf<Int>()
+                for (i in 0 until c1) p1l.add(player1[i])
+                val p2l = mutableListOf<Int>()
+                for (i in 0 until c2) p2l.add(player2[i])
+                game++
+                winner = spaceCards(p1l, p2l)
             } else winner = if (c1 > c2) 1 else 2
 
-            p1Set.add(player1); p2Set.add(player2)
+            p1Set.add(player1.joinToString()); p2Set.add(player2.joinToString())
 
+            val n1 = stack[stack.size - 1]
+            val n2 = stack[stack.size - 2]
             when (winner) {
-                1 -> player1.addAll(stack.sortedDescending())
-                2 -> player2.addAll(stack.sortedDescending())
-                else -> return winner
+                1 -> {
+                    player1.add(n1); player1.add(n2)
+                }
+                2 -> {
+                    player2.add(n1); player2.add(n2)
+                }
+                else -> {
+                    return winner
+                }
             }
+            stack.removeLast()
+            stack.removeLast()
         }
-        if(stack.isEmpty()){
+        game--
+        if (stack.isEmpty()) {
             val winner = if (player1.isEmpty()) player2 else player1
             return winner.reversed().withIndex().sumBy { (idx, it) -> it * (idx + 1) }
         } else {
-            if(player1.isEmpty()) return 2
+            if (player1.isEmpty()) return 2
             else return 1
         }
     }
