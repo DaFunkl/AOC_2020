63e1d15ae8197d599122a15712fb08a9452aa7a7
Alexey Gasevic <alexey.gasevic@vv.de>
2020-12-23 07:59:04 +0100

    qs

--- src/main/kotlin/days/Day23.kt
+++ src/main/kotlin/days/Day23.kt
@@ -1,13 +1,57 @@
 package days
 
 class Day23 : Day(23) {
+    val data = inputString.map { Character.getNumericValue(it) }
+
+    data class Node(var value: Int) {
+        lateinit var prev: Node
+        lateinit var next: Node
+
+        fun toStr(delimiter: String = ", "): String {
+            var str = "$value$delimiter"
+            var n = next
+            while (n.value != value) {
+                str += "${n.value}$delimiter"
+                n = n.next
+            }
+            return str
+        }
+
+        fun remove(): Node{
+            prev.next = next
+            next.prev = prev
+            return this
+        }
+    }
 
     override fun partOne(): Any {
+        var current = fetchLL(data)
+        println(current.toStr())
+        for (i in 0 until 10) {
+            val removed = mutableListOf<Node>()
+            for(r in 0 until 3){
+                removed.add(current.next.remove())
+            }
+            var dest
+        }
         return -1
     }
 
-
     override fun partTwo(): Any {
         return -1
+    }
+
+    fun fetchLL(input: List<Int>): Node {
+        var start = Node(input[0])
+        var last: Node = start
+        for (i in 1 until input.size) {
+            var next = Node(input[i])
+            next.prev = last
+            last.next = next
+            last = next
+        }
+        start.prev = last
+        last.next = start
+        return start
     }
 }
