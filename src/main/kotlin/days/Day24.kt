37657c6f6c4fab1b84533f74d6091233e7e79cb2
Alexey Gasevic <alexey.gasevic@vv.de>
2020-12-24 08:34:35 +0100

    qs

--- src/main/kotlin/days/Day24.kt
+++ src/main/kotlin/days/Day24.kt
@@ -1,12 +1,73 @@
 package days
 
 class Day24 : Day(24) {
+    val data = inputList.map { replaceDirs(it) }
+    val set = mutableSetOf<Pair<Int, Int>>()
+    val dirs = Direction.values().map { it.dir }
 
     override fun partOne(): Any {
-        return -1
+        data.forEach { ot ->
+            var pos = Pair(0, 0)
+            ot.forEach {
+                pos += when (it) {
+                    'e' -> Direction.E.dir
+                    'w' -> Direction.W.dir
+                    'k' -> Direction.SE.dir
+                    'm' -> Direction.SW.dir
+                    'n' -> Direction.NW.dir
+                    'p' -> Direction.NE.dir
+                    else -> throw Exception("Unknown IT: $it")
+                }
+            }
+            if (set.contains(pos)) set.remove(pos) else set.add(pos)
+        }
+        return set.size
     }
 
     override fun partTwo(): Any {
-        return -1
+        var cState = mutableSetOf<Pair<Int, Int>>()
+        cState.addAll(set)
+        for (i in 1..100) {
+            val nState = mutableSetOf<Pair<Int, Int>>()
+            val todos = mutableSetOf<Pair<Int, Int>>()
+            cState.forEach { todos.add(it); dirs.forEach { d -> todos.add(it + d) } }
+            todos.forEach {
+                val adj = countAdj(it, cState)
+                if (
+                    (cState.contains(it) && adj == 1) ||
+                    (!cState.contains(it) && adj == 2)
+                ) nState.add(it)
+            }
+            cState = nState
+            if (i < 10 || i % 10 == 0) println("Day $i: ${cState.size}")
+        }
+        println("Day : ${cState.size}")
+        return cState.size
     }
+
+    fun countAdj(p: Pair<Int, Int>, s: Set<Pair<Int, Int>>): Int =
+        dirs.map { it + p }.filter { s.contains(it) }.count()
+
+    fun replaceDirs(str: String): String {
+        return str.replace("se", "k")
+            .replace("sw", "m")
+            .replace("nw", "n")
+            .replace("ne", "p")
+    }
+
+    operator fun Pair<Int, Int>.plus(o: Pair<Int, Int>): Pair<Int, Int> =
+        Pair(this.first + o.first, this.second + o.second)
+
+    fun Pair<Int, Int>.plus(o: Direction): Pair<Int, Int> = this + o.dir
+
+    enum class Direction(val dir: Pair<Int, Int>) {
+        E(Pair(2, 0)),
+        W(Pair(-2, 0)),
+        SE(Pair(1, 1)), // k
+        SW(Pair(-1, 1)), // m
+        NW(Pair(-1, -1)), // n
+        NE(Pair(1, -1)) // p
+    }
+
+
 }
